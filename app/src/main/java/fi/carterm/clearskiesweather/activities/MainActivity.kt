deb74ece68740544de31bfa695517b7309c46c6e
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2021-09-20 19:59:05 +0300

    testing CI/CD

--- app/src/main/java/fi/carterm/clearskiesweather/activities/MainActivity.kt
+++ app/src/main/java/fi/carterm/clearskiesweather/activities/MainActivity.kt
@@ -2,11 +2,14 @@ package fi.carterm.clearskiesweather.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import fi.carterm.clearskiesweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("test", "testing ci/cd")
     }
 }
