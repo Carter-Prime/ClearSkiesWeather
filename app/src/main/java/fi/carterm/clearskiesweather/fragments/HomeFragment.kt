6d46434423d306285c91a462fae6a969d4be847e
KlenJavor <patricie.suppala@gmail.com>
2021-09-24 03:18:20 +0300

    light sensor getting data

--- app/src/main/java/fi/carterm/clearskiesweather/fragments/HomeFragment.kt
+++ app/src/main/java/fi/carterm/clearskiesweather/fragments/HomeFragment.kt
@@ -1,17 +1,27 @@
 package fi.carterm.clearskiesweather.fragments
 
+
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import fi.carterm.clearskiesweather.R
 import fi.carterm.clearskiesweather.databinding.FragmentHomeBinding
 import fi.carterm.clearskiesweather.viewmodels.HomeViewModel
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment: Fragment(R.layout.fragment_home), SensorEventListener {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var sensorManager: SensorManager
+    private var brightness: Sensor? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,5 +32,42 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         homeViewModel.weatherData.observe(viewLifecycleOwner){
             Log.d("dbApp", "Weather Data: $it")
         }
+
+        setUpSensor()
+    }
+    private fun setUpSensor() {
+        sensorManager = activity!!.getSystemService(SENSOR_SERVICE) as SensorManager
+        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+            val light1 = event.values[0]
+            //val sensorLight = binding.tvSensorLight.text
+            val sensorLight: TextView = activity!!.findViewById(R.id.tv_sensor_light)
+            sensorLight.text =  "Sensor: $light1\n${brightness(light1)}"
+            //  pb.setProgressWithAnimation(light1)
+        }
+    }
+
+    private fun brightness(brightness: Float): String {
+
+        return when (brightness.toInt()) {
+            0 -> "Pitch black"
+            in 1..10 -> "Dark"
+            in 11..50 -> "Grey"
+            in 51..5000 -> "Normal"
+            in 5001..25000 -> "Incredibly bright"
+            else -> "This light will blind you"
+        }
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Register a listener for the sensor.
+        sensorManager.registerListener(this, brightness, SensorManager.SENSOR_DELAY_NORMAL)
     }
 }
