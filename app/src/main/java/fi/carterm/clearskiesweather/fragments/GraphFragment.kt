5fb7521f21cc3873598abf8c27ee1eb2d97a9d02
KlenJavor <patricie.suppala@gmail.com>
2021-09-29 16:26:56 +0300

    graph and one menu added

--- app/src/main/java/fi/carterm/clearskiesweather/fragments/GraphFragment.kt
+++ app/src/main/java/fi/carterm/clearskiesweather/fragments/GraphFragment.kt
@@ -2,13 +2,24 @@ package fi.carterm.clearskiesweather.fragments
 
 
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.util.Log
 import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import fi.carterm.clearskiesweather.R
 import fi.carterm.clearskiesweather.databinding.FragmentGraphBinding
 import fi.carterm.clearskiesweather.viewmodels.GraphViewModel
+import com.jjoe64.graphview.series.LineGraphSeries
+
+
+
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
 
 
 class GraphFragment : Fragment(R.layout.fragment_graph) {
@@ -27,9 +38,49 @@ class GraphFragment : Fragment(R.layout.fragment_graph) {
             Log.d("dbApp", "Weather Data: $it")
         }
 
+        val cardView = view.findViewById<CardView>(R.id.base_cardview)
+        val arrow = view.findViewById<ImageButton>(R.id.arrow_button)
+        val hiddenView = view.findViewById<LinearLayout>(R.id.hidden_view)
 
+        arrow.setOnClickListener(View.OnClickListener {
+            // If the CardView is already expanded, set its visibility
+            //  to gone and change the expand less icon to expand more.
+            if (hiddenView.getVisibility() == View.VISIBLE) {
 
+                // The transition of the hiddenView is carried out
+                //  by the TransitionManager class.
+                // Here we use an object of the AutoTransition
+                // Class to create a default transition.
+                TransitionManager.beginDelayedTransition(
+                    cardView,
+                    AutoTransition()
+                )
+                hiddenView.setVisibility(View.GONE)
+                arrow.setImageResource(R.drawable.ic_baseline_expand_more_24)
+            } else {
+                TransitionManager.beginDelayedTransition(
+                    cardView,
+                    AutoTransition()
+                )
+                hiddenView.setVisibility(View.VISIBLE)
+                arrow.setImageResource(R.drawable.ic_baseline_expand_less_24)
+            }
+        })
+
+        val graph = view.findViewById(R.id.graph) as GraphView
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series)
     }
 
-
 }
+
+
+
