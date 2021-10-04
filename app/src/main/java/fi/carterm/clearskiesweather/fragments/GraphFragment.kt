a30531827e6cf5ddb982c58ff024d9e0787cbf77
KlenJavor <patricie.suppala@gmail.com>
2021-10-04 11:52:27 +0300

    date pickers

--- app/src/main/java/fi/carterm/clearskiesweather/fragments/GraphFragment.kt
+++ app/src/main/java/fi/carterm/clearskiesweather/fragments/GraphFragment.kt
@@ -5,12 +5,10 @@ import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import fi.carterm.clearskiesweather.R
 import fi.carterm.clearskiesweather.databinding.FragmentGraphBinding
@@ -18,6 +16,12 @@ import fi.carterm.clearskiesweather.viewmodels.GraphViewModel
 import com.jjoe64.graphview.series.LineGraphSeries
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
+import com.google.android.material.datepicker.MaterialDatePicker
+import android.text.Editable
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+
 
 class GraphFragment : Fragment(R.layout.fragment_graph) {
 
@@ -30,6 +34,39 @@ class GraphFragment : Fragment(R.layout.fragment_graph) {
         binding = FragmentGraphBinding.bind(view)
         val viewModel: GraphViewModel by viewModels()
         graphViewModel = viewModel
+         val fragmentManager = (activity as FragmentActivity).supportFragmentManager
+
+        //startDatePicker
+        val startDatePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Event Start Date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(CalendarConstraints.Builder()
+                    .setValidator(DateValidatorPointForward.now()).build())
+                .build()
+        binding.etEventStartDate.setOnClickListener {
+            startDatePicker.show(fragmentManager, "tag")
+        }
+        startDatePicker.addOnPositiveButtonClickListener {
+            val date = startDatePicker.headerText
+            binding.etEventStartDate.text = Editable.Factory.getInstance().newEditable(date)
+        }
+
+        //endDatePicker
+        val endDatePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Event End Date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(CalendarConstraints.Builder()
+                    .setValidator(DateValidatorPointForward.now()).build())
+                .build()
+        binding.etEventEndDate.setOnClickListener {
+            endDatePicker.show(fragmentManager, "tag")
+        }
+        endDatePicker.addOnPositiveButtonClickListener {
+            val date = endDatePicker.headerText
+            binding.etEventEndDate.text = Editable.Factory.getInstance().newEditable(date)
+        }
 
         graphViewModel.weatherData.observe(viewLifecycleOwner) {
             Log.d("dbApp", "Weather Data: $it")
@@ -77,20 +114,24 @@ class GraphFragment : Fragment(R.layout.fragment_graph) {
         )
         graph.addSeries(series)
 
-        //PICK DATE BUTTONS
-        val startDateButton = view.findViewById<Button>(R.id.btn_starDate)
-        val endDateButton = view.findViewById<Button>(R.id.btn_endDate)
-
-
-        startDateButton.setOnClickListener {
-            showDatePickerDialog(it)
-        }
-
     }
-    fun showDatePickerDialog(v: View) {
-        val newFragment = DatePickerFragment()
-        val supportFragmentManager:FragmentManager
-        newFragment.show(supportFragmentManager, "datePicker")
+
+
+    private fun getMonthFormat(month: Int): String {
+        if (month == 1) return "JAN"
+        if (month == 2) return "FEB"
+        if (month == 3) return "MAR"
+        if (month == 4) return "APR"
+        if (month == 5) return "MAY"
+        if (month == 6) return "JUN"
+        if (month == 7) return "JUL"
+        if (month == 8) return "AUG"
+        if (month == 9) return "SEP"
+        if (month == 10) return "OCT"
+        if (month == 11) return "NOV"
+        return if (month == 12) "DEC" else "JAN"
+
+        //default should never happen
     }
 }
 
