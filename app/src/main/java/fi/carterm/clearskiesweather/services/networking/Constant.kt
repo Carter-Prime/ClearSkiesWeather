87ec4364853e2d99d4c5aa2ffd3e5f3408fb5e7e
KlenJavor <patricie.suppala@gmail.com>
2021-10-15 03:46:18 +0300

    date selector backward

--- app/src/main/java/fi/carterm/clearskiesweather/services/networking/Constant.kt
+++ app/src/main/java/fi/carterm/clearskiesweather/services/networking/Constant.kt
@@ -1,5 +1,5 @@
 package fi.carterm.clearskiesweather.services.networking
 
 object Constant {
-    const val API_KEY = "3b652131c8bd2ab5f62f1959b63267f3"
+    const val API_KEY = "Your API KEY GOES HERE"
 }
